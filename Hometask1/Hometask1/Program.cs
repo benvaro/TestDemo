d12b24bba9f94f5c2c6d78f8454f99a08c880f6b
benvaro <benvaro91@gmail.com>
2020-04-29 18:06:22 +0300

    delete hello

--- Hometask1/Hometask1/Program.cs
+++ Hometask1/Hometask1/Program.cs
@@ -10,7 +10,8 @@ namespace Hometask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!!!!");Console.WriteLine("Hello world!!!!");
+            Console.WriteLine("Hello world!!!!");
+
         }
     }
 }
